a0f0e55f811154b4ae59ed5acd4024be5c3d4c5f
unknown <arnaud.defrancq@laposte.net>
2023-08-24 18:30:58 +0200

    exercice 4 a corriger

--- php/Exercice/II.4/test.php
+++ php/Exercice/II.4/test.php
@@ -6,12 +6,21 @@ $candidat3 = readline('score 3 : ');
 $candidat4 = readline('score 4 : ');
 
 
+$election = [$candidat2, $candidat3, $candidat4];
+
+
 
 
 if ($candidat1 > 50) {
     echo 'élu';
 } elseif ($candidat1 >= 12.5) {
-    
+    foreach ($election as $score) {
+        if ($candidat1 > $score) {
+            echo 'favorable';
+        } elseif ($candidat1 < $score) {
+            echo 'défavorable';
+        }
+    }
 } else {
     echo 'ne passe pas le premier tour';
 }
