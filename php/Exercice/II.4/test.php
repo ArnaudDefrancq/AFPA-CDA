953fd714f9c3d4f4a477fdfea65ad49f8937850f
unknown <arnaud.defrancq@laposte.net>
2023-08-27 20:26:31 +0200

    Ajout de le correction II.4

--- php/Exercice/II.4/test.php
+++ php/Exercice/II.4/test.php
@@ -10,7 +10,6 @@ $election = [$candidat2, $candidat3, $candidat4];
 
 
 
-
 if ($candidat1 > 50) {
     echo 'élu';
 } elseif ($candidat1 >= 12.5) {
@@ -23,4 +22,37 @@ if ($candidat1 > 50) {
     }
 } else {
     echo 'ne passe pas le premier tour';
+}
+
+
+
+// Correction 
+
+$age = readLine('Age : ');
+$permis = readLine('Années de permis : ');
+$accident = readLine('Nombre d\'accidents : ');
+$gold = readLine('Client depuis plus d\'un an ? : ');
+
+$score = 0;
+
+$couleur = ['rouge', 'orange', 'verte', 'bleue'];
+
+if ($age > 25) {
+    $score++;
+}
+
+if ($permis > 2) {
+    $score++;
+}
+
+$score -= $accident;
+
+if ($gold && $score >= 0) {
+    $score++;
+}
+
+if ($score >= 0) {
+    echo 'Vous avez droit à l\'assurance de couleur ' . $couleur[$score];
+} else {
+    echo 'Vous ne pouvez pas souscrire à une assurance';
 }
