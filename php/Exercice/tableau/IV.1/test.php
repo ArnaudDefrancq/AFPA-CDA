e5e34fdbce3dadaca1ba61d4d51105bfa1804c8a
arnaud <arnaud.defrancq@laposte.net>
2023-08-28 17:10:26 +0200

    finir tableau 2D

--- php/Exercice/tableau/IV.1/test.php
+++ php/Exercice/tableau/IV.1/test.php
@@ -8,20 +8,65 @@
 // Si 1 => ajouter des valeurs à X
 // Si 2 => Ajouter des valeurs à X et Y
 
-function saisirTableau(int $dimension)
+
+// $test = [
+//     [
+//         'x' => 1,
+//         'y' => 1
+//     ],
+//     [
+//         'x' => 2,
+//         'y' => 3
+//     ]
+// ];
+
+
+// var_dump($test);
+
+// function saisirTableau(int $dimension)
 {
-    $array = [];
-};
-
-saisirTableau(2);
-
-
     // $array = [];
-    // do {
+
+    // if ($dimension == 2) {
+    //     $arrayDim = [];
     //     do {
-    //         $value = readline('Ajouter une valeur dans un tableau : ');
-    //     } while (!preg_match("/^[0-9]+$/", $value)); // check valeur entrer
-    //     array_push($array, $value);
-    // } while ($value != 0); // Si 0 termine la boucle
-    // array_pop($array); // retire le 0
-    // var_dump($array);
+
+
+    //         do {
+    //             $valueX = readline('Ajouter une valeur X : ');
+    //             $valueY = readline('Ajouter une valeur Y : ');
+    //         } while (!preg_match("/^[0-9]+$/", $valueX, $valueY));
+
+
+    //         $arrayDim["x"] = $valueX;
+    //         $arrayDim["y"] = $valueY;
+    //     } while ($valueX != 0 && $valueY != 0);
+
+    //     var_dump($arrayDim);
+}
+
+// array_push($array, $arrayDim);
+
+// var_dump($array);
+// };
+
+// saisirTableau(2);
+
+
+
+
+function saisirTab()
+{
+
+    $array = [];
+    do {
+        do {
+            $value = readline('Ajouter une valeur dans un tableau : ');
+        } while (!preg_match("/^[0-9]+$/", $value)); // check valeur entrer
+        array_push($array, $value);
+    } while ($value != 0); // Si 0 termine la boucle
+    array_pop($array); // retire le 0
+    var_dump($array);
+}
+
+saisirTab();
--- php/Exercice/tableau/IV.3/test.php
+++ php/Exercice/tableau/IV.3/test.php
@@ -1,19 +1,39 @@
 <?php
 //  tableau multidimensionnel
 
-/*
-    exemple : 
-        array = [
-            [
-                x => 1,
-                y => 1
-            ],
-            [
-                x => 2,
-                y => 2,
-            ],
-            [
-                ...
-            ]
-        ]
-*/
+
+// $arrayTest = [
+//     [
+//         'x' => 1,
+//         'y' => 1
+//     ],
+//     [
+//         'x' => 2,
+//         'y' => 2,
+//     ]
+
+// ];
+
+// var_dump($arrayTest);
+
+
+// retirer derniere valeur
+function saisirTab()
+{
+
+    $array = [];
+    do {
+
+        do {
+
+            $value = (int) readline('Ajouter une valeur dans un tableau : ');
+        } while (!preg_match("/^[0-9]+$/", $value)); // check valeur entrer
+
+        array_push($array, ['x' => $value, 'y' => $value]);
+        array_pop($array); // retire le 0
+    } while ($value != 0); // Si 0 termine la boucle
+
+    var_dump($array);
+}
+
+saisirTab();
