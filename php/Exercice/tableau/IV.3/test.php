9c5c1ffe7f5cb58111cda49f2e115589ff74c6ff
arnaud <arnaud.defrancq@laposte.net>
2023-08-28 17:15:48 +0200

    fx tab 2D fini

--- php/Exercice/tableau/IV.3/test.php
+++ php/Exercice/tableau/IV.3/test.php
@@ -1,36 +1,15 @@
 <?php
 //  tableau multidimensionnel
 
-
-// $arrayTest = [
-//     [
-//         'x' => 1,
-//         'y' => 1
-//     ],
-//     [
-//         'x' => 2,
-//         'y' => 2,
-//     ]
-
-// ];
-
-// var_dump($arrayTest);
-
-
-// retirer derniere valeur
 function saisirTab()
 {
-
     $array = [];
     do {
-
         do {
-
             $value = (int) readline('Ajouter une valeur dans un tableau : ');
         } while (!preg_match("/^[0-9]+$/", $value)); // check valeur entrer
 
-        array_push($array, ['x' => $value, 'y' => $value]);
-        array_pop($array); // retire le 0
+        if ($value != 0) array_push($array, ['x' => $value, 'y' => $value]);
     } while ($value != 0); // Si 0 termine la boucle
 
     var_dump($array);
