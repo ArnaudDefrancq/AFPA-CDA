310bd87d3f5fbe298ffd521eaca2ef5b67765d77
unknown <arnaud.defrancq@laposte.net>
2023-09-23 18:53:24 +0200

    mouvement du cube en fonction du cube blanc ok

--- JS/taquin/app.js
+++ JS/taquin/app.js
@@ -8,19 +8,65 @@
 
 const allBtn = document.querySelectorAll(".btn");
 allBtn.forEach((btn) => {
-  btn.addEventListener("click", swapCube);
+  btn.addEventListener("click", game);
 });
 
 //---------------------------------------Function----------------------------------------------------
+/**
+ * Swap le btn clicke avec le btn blanc
+ * @param {*} event
+ */
 function swapCube(event) {
-  let btnClick = event.target; // recup le btn clicker
+  let btnClick = event.target;
 
   allBtn.forEach((btn) => {
     if (btn.innerHTML == "") {
+      // check si le btn est vide
       btn.innerHTML = btnClick.innerHTML;
       btn.classList.remove("btn-blanc");
       btnClick.classList.add("btn-blanc");
       btnClick.innerHTML = "";
     }
   });
+}
+
+/**
+ * Permet de localiser la position du cube blanc
+ */
+function localisationCubeBlanc() {
+  let btnBlanc;
+  allBtn.forEach((btn) => {
+    if (btn.classList.length == 2) btnBlanc = btn;
+  });
+  return btnBlanc;
+}
+
+function game(event) {
+  let btnClickLigne = event.target.dataset.ligne;
+  let btnClickColonne = event.target.dataset.colonne;
+
+  let btnClick = event.target;
+
+  let positionCubeBlanc = localisationCubeBlanc();
+
+  console.log(
+    btnClickColonne - positionCubeBlanc.dataset.colonne,
+    positionCubeBlanc.dataset.ligne - btnClickLigne
+  );
+
+  if (
+    (btnClickColonne - positionCubeBlanc.dataset.colonne == 0 &&
+      positionCubeBlanc.dataset.ligne - btnClickLigne == 1) ||
+    (btnClickColonne - positionCubeBlanc.dataset.colonne == 0 &&
+      positionCubeBlanc.dataset.ligne - btnClickLigne == -1) ||
+    (btnClickLigne - positionCubeBlanc.dataset.ligne == 0 &&
+      positionCubeBlanc.dataset.colonne - btnClickColonne == 1) ||
+    (btnClickLigne - positionCubeBlanc.dataset.ligne == 0 &&
+      positionCubeBlanc.dataset.colonne - btnClickColonne == -1)
+  ) {
+    positionCubeBlanc.innerHTML = btnClick.innerHTML;
+    positionCubeBlanc.classList.remove("btn-blanc");
+    btnClick.classList.add("btn-blanc");
+    btnClick.innerHTML = "";
+  }
 }
