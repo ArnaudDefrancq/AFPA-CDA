283a5796c8ebb825b7392b2cce67c9721e7ada57
ArnaudDefrancq <arnaud.defrancq@laposte.net>
2023-10-26 17:17:24 +0200

    AFPA-SOIR

--- 03 - Interface/C#/PremierTest/PremierTest/Program.cs
+++ 03 - Interface/C#/PremierTest/PremierTest/Program.cs
@@ -10,6 +10,75 @@ namespace PremierTest
     {
         static void Main(string[] args)
         {
+            // Exercice 1 -> Variable
+            //string chaineSaisie;
+            //Console.WriteLine("Entrez votre nom :");
+            //chaineSaisie = Console.ReadLine();
+            //Console.WriteLine("Vous avez saisie : " + chaineSaisie);
+
+            // Exercice 2 
+            //string chiffreChoisie;
+            //int nb;
+            //Console.WriteLine("Entrez un chiffre :");
+            //chiffreChoisie = Console.ReadLine();
+            //nb = Convert.ToInt32(chiffreChoisie);
+            //Console.WriteLine("Vous avez saisie : " + nb);
+
+            // Exercice 3 -> Entier
+            //string chiffreChoisi_1;
+            //string chiffreChoisi_2;
+            //int nb_1;
+            //int nb_2;
+            //Console.WriteLine("Entrez chiffre 1 :");
+            //chiffreChoisi_1 = Console.ReadLine();
+            //nb_1 = Convert.ToInt32(chiffreChoisi_1);
+            //Console.WriteLine("Entrez chiffre 2 :");
+            //chiffreChoisi_2 = Console.ReadLine();
+            //nb_2 = Convert.ToInt32(chiffreChoisi_2);
+            //Console.WriteLine("chiffre 1 : "+ nb_1);
+            //Console.WriteLine("chiffre 2 : "+ nb_2);
+            //Console.WriteLine("somme : "+ (nb_2 + nb_1));
+            //Console.WriteLine("quotient : "+ nb_1 / nb_2);
+
+            // Exercice 4 -> Flottants
+            //string floatChoisie;
+            //float nb;
+            //Console.WriteLine("Entrez un float :");
+            //floatChoisie = Console.ReadLine();
+            //nb = (float)Convert.ToDecimal(floatChoisie);
+            //Console.WriteLine("float :" + nb);
+
+            // Exercice 5
+            //    string saisirChiffre;
+            //    float nb_1;
+            //    float test = 0;
+            //    float tour = 0;
+            //    for (int i = 0; i < 3 ; i++)
+            //    {
+            //        Console.WriteLine("Saisir chiffre :");
+            //        saisirChiffre = Console.ReadLine(); // saisie note
+            //        nb_1 =  (float)Convert.ToDecimal(saisirChiffre); // ajout a la note 1
+            //        test += nb_1 ; // Ajout de la note
+            //        tour += 1;             
+            //    }
+            //        Console.WriteLine("float :" + (test/tour));
+
+            // Exercice 6
+            //string chiffreChoisie;
+            //double longeur;
+            //double largeur;
+            //Console.WriteLine("Entrez un longeur :");
+            //chiffreChoisie = Console.ReadLine();
+            //longeur = Convert.ToDouble(chiffreChoisie);
+            //Console.WriteLine("Entrez un largeur :");
+            //chiffreChoisie = Console.ReadLine();
+            //largeur = Convert.ToDouble(chiffreChoisie);
+            //Console.WriteLine("Périmètre rectangle : " + (largeur * longeur));
+
+            // Exercice 7 -> Caractères
+            char variable = 'a';
+            UnicodeEncoding(variable);
+
         }
     }
 }
--- 03 - Interface/C#/PremierTest/PremierTest/Properties/AssemblyInfo.cs
+++ 03 - Interface/C#/PremierTest/PremierTest/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("c8a8add2-ad3a-40c1-a2ab-9de2fc852541")]
+[assembly: Guid("e569bb5a-17a6-459e-aedf-e3899ce7c04b")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
