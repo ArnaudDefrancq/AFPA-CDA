7589314c84c4226a9f6426e01f3c67122cd1c484
ArnaudDefrancq <arnaud.defrancq@laposte.net>
2023-10-31 11:58:43 +0100

    Fin exo Boucles

--- 03 - Interface/C#/Variable/Boucles/Program.cs
+++ 03 - Interface/C#/Variable/Boucles/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Boucles
 {
@@ -75,6 +77,180 @@ namespace Boucles
             //Console.WriteLine("La factorielle de " + nbFactorielle + " est " + somme);
 
             // 4.3 - Choix de la boucle la plus appropriée
+            // Exercice 6 - Table de multiplication
+            // int nbChoisi;
+
+            // do
+            // {
+            //     Console.WriteLine("Choisir un nb et vous aurez sa table de multiplication : ");
+            // } while (!int.TryParse(Console.ReadLine(), out nbChoisi));
+
+            // for (int i = 0; i < 11; i++)
+            // {
+            //     Console.WriteLine(nbChoisi + " * " + i + " = " + (nbChoisi * i));
+            // }
+
+            // Exercice 7 - Tables de multiplications
+
+            //Console.Write("/|");
+            //for (int i = 0; i < 11; i++)
+            //{
+            //    Console.Write(i + "|");
+            //}
+            //Console.Write('\n');
+            //for (int i = 0; i < 11; i++)
+            //{
+            //    Console.Write(i);
+            //    Console.Write("|");
+            //    for (int j = 0; j < 11; j++)
+            //    {
+            //        int result = i * j;
+            //        Console.Write(result + "|");
+            //    }
+            //    Console.Write('\n');
+            //}
+
+            // Exercice 8 - Puissance
+            //int b, n;
+            //String valeurB, valeurN;
+
+            //do
+            //{
+            //    Console.WriteLine("Saisir un nombre 'b' : ");
+            //    valeurB = Console.ReadLine();
+            //    Console.WriteLine("Saisir un nombre 'n' positif : ");
+            //    valeurN = Console.ReadLine();
+            //} while (!int.TryParse(valeurB, out b) || !int.TryParse(valeurN, out n) || n < 0);
+
+            //Double result = Math.Pow(b, n);
+
+            //Console.WriteLine("Resultat de " + b + " puissance " + n + " = " + result);
+
+            // Exercice 9 -Joli carré
+            //int saisirNb;
+
+            //do
+            //{
+            //    Console.WriteLine("Saisir un nb (positif) : ");
+            //} while (!int.TryParse(Console.ReadLine(), out saisirNb) || saisirNb < 0);
+            //Console.WriteLine("n = " + saisirNb);
+
+            //for (int i = 0; i < saisirNb; i++)
+            //{
+            //    Console.Write(" x ");
+            //    for (int j = 0; j < saisirNb - 1; j++)
+            //    {
+            //        Console.Write(" x ");
+            //    }
+            //    Console.Write("\n");
+            //}
+
+            // 4.4 - Extension de la calculatrice
+            // Exercice 10 - Calculatrice de poche / Exercice 11 - Puissance / Exercice 12 - Opérations unaires
+            Double a, b;
+            Char op;
+            String valeurA, valeurB;
+
+            static bool checkOp(Char o) // Fonction qui check le signe
+            {
+                List<Char> operateur = new List<Char> { '+', '-', '*', '/', '=', '$', 'r', 'f' };
+                foreach (Char c in operateur)
+                {
+                    if (o == c)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            };
+            do
+            {
+                Console.WriteLine("Saisir la valeur de a : ");
+                valeurA = Console.ReadLine();
+
+            } while (!Double.TryParse(valeurA, out a));
+
+
+
+            do
+            {
+                Double result;
+                // Demande l'opérateur
+                do
+                {
+                    Console.WriteLine("Saisir un signe opérateur (' = ' pour sortir) : ");
+                    op = Console.ReadLine()[0];
+                } while (!checkOp(op));
+
+                if (op != '=')
+                {
+                    if (op == 'r')
+                    {
+                        result = Math.Sqrt(a);
+                        Console.WriteLine(result);
+                        a = result;
+                    }
+                    else if (op == 'f')
+                    {
+                        Double nbTour = a;
+                        Double facto = 1;
+                        for (Double i = 1; i <= nbTour; i++)
+                        {
+                            facto *= i;
+                        }
+                        Console.WriteLine(facto);
+                        a = facto;
+                    }
+                    else
+                    {
+                        do
+                        {
+                            Console.WriteLine("Saisir un nombre : ");
+                            valeurB = Console.ReadLine();
+                        } while (!Double.TryParse(valeurB, out b));
+
+
+                        // Les opérations de calculs
+                        switch (op)
+                        {
+                            case '-':
+                                result = (a - b);
+                                Console.WriteLine(result);
+                                a = result;
+                                break;
+                            case '*':
+                                result = (a * b);
+                                Console.WriteLine(result);
+                                a = result;
+                                break;
+                            case '/':
+                                if (b != 0)
+                                {
+                                    result = (a / b);
+                                    Console.WriteLine(result);
+                                    a = result;
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Diviser par 0 ! T'es pas malade !");
+                                    break;
+                                };
+                            case '$':
+                                result = Math.Pow(a, Math.Floor(b));
+                                Console.WriteLine(result);
+                                a = result;
+                                break;
+                            default:
+                                result = (a + b);
+                                Console.WriteLine(result);
+                                a = result;
+                                break;
+
+                        }
+                    }
+                }
+            } while (op != '=');
         }
     }
 }
