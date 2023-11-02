41d8bbe3e09020fbda74fdb4fa8892c18c5d2f5c
ArnaudDefrancq <arnaud.defrancq@laposte.net>
2023-11-02 17:06:12 +0100

    AFPA soir

--- 03 - Interface/C#/Variable/Tableaux/Program.cs
+++ 03 - Interface/C#/Variable/Tableaux/Program.cs
@@ -67,27 +67,147 @@
 //Console.WriteLine("Votre chiffre est " + dansTab + " présent dans le tableau");
 
 // 6.3 - Indices
-// Exercice 8 - Permutation circulaire
-int[] tab;
-tab = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-int elmt;
+// Exercice 8 - Permutation circulaire avec deux tableaux
+//int[] newTab = new int[10];
+//int lastElmt = tab[tab.Length - 1];
+//newTab[0] = lastElmt;
+//for (int i = 1; i < newTab.Count(); i++)
+//{
+//    newTab[i] = tab[i - 1];
+//}
 
-do
-{
-    Console.WriteLine("Saisir un chiffre pour effectuer la permutation circulaire : ");
-} while (!int.TryParse(Console.ReadLine(), out elmt));
+//foreach (int i in newTab)
+//{
+//    Console.WriteLine(i);
+//}
+// Exercice 9 - Permutation circulaire sans deuxième tableau
+//int[] tab;
+//tab = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-if (tab.Contains(elmt))
-{
+//for (int i = 0; i < tab.Count() - 1; i++)
+//{
+//	int premier = tab[0];
+//	for (int j = 0; j < tab.Count() - 1; j++)
+//	{
+//		tab[j] = tab[j + 1];
+//	}
+//	tab[tab.Length - 1] = premier;
+//}
+
+//int dernier = tab[tab.Count() - 1];
+
+//for (int i = tab.Count() - 1; i > 0; i--)
+//{
+//	tab[i] = tab[i - 1];
+
+//}
+
+//tab[0] = dernier;
+//foreach (int i in tab)
+//{
+//	Console.WriteLine(i);
+//}
+
+// Exercice 10 - Miroir
+//Array.Reverse(tab);
+//foreach (int i in tab)
+//{
+//    Console.WriteLine(i);
+//}
+
+// 6.4 - Recherche séquentielle
+// Exercice 11 - Modification du tableau
+//Double[] tab;
+//tab = new Double[20];
+
+//for (int i = 1; i < tab.Count(); i++)
+//{
+//	Double carre = Math.Pow(i, 2);
+//	tab[i] = (carre % 17);
+//}
+
+//foreach (int i in tab)
+//{
+//	Console.WriteLine(i);
+//}
+
+// Exercice 12 - Min / Max
+//Double max = tab[0];
+//Double min = tab[0];
 
 
+//for (int i = 1; i < tab.Count(); i++)
+//{
+//    if (max <= tab[i])
+//    {
+//        max = tab[i];
+//    }
 
-    foreach (int item in tab)
-    {
-        Console.WriteLine(item);
-    }
-}
-else
-{
-    Console.Write(elmt + " n'est pas dans le tableau");
-}
+//    if (min >= tab[i])
+//    {
+//        min = tab[i];
+//    }
+//}
+
+//Console.WriteLine(max + " est la valeur la plus grande et " + min + " est la valeur la plus faible");
+
+// Exercice 13 - Recherche séquentielle
+//Double x;
+//do
+//{
+//    Console.WriteLine("Saisir une valeur x : ");
+//} while (!Double.TryParse(Console.ReadLine(), out x));
+
+//for (int i = 0; i < tab.Length; i++)
+//{
+//    if (tab[i] == x)
+//    {
+//        Console.WriteLine("Il se trouve au index : " + i);
+//    }
+//}
+
+// Exercice 14 - recherche séquentielle avec stockage des indices (tab dans l'exercice 11)
+//Double[] newTab = { };
+//Double x;
+//do
+//{
+//	Console.WriteLine("Saisir une valeur x : ");
+//} while (!Double.TryParse(Console.ReadLine(), out x));
+
+//for (int i = 0; i < tab.Count(); i++)
+//{
+//	if (tab[i] == x)
+//	{
+//		Double item = Convert.ToDouble(i);
+//		newTab = newTab.Append(item).ToArray();
+//	}
+//}
+
+//if (newTab.Count() == 0)
+//{
+//	Console.WriteLine(x + " n'est pas dans le tableau");
+//}
+//else
+//{
+//	Console.WriteLine(x + " est dans le tableau au(x) index : ");
+//	foreach (Double item in newTab)
+//	{
+//		Console.WriteLine("index : " + item);
+//	}
+//}
+
+// 6.5 - Morceaux choisis
+// Exercice 15 - Pièces de monnaie
+//Double[] valeurPiece = { 0.5, 0.2, 0.1, 0.05, 0.02, 0.01 };
+//Double[] nombrePiece = { };
+//Double somme;
+
+//do
+//{
+//	Console.WriteLine("Entrez une somme comprise entre 0 et 0,99");
+//} while (!Double.TryParse(Console.ReadLine(), out somme) || somme > 0.99 || somme < 0);
+
+//while (somme != 0)
+//{
+
+//}
