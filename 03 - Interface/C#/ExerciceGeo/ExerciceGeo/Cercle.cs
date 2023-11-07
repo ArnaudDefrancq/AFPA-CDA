a3cb2517a450682fe2b8e3c75524efbb0598c19f
ArnaudDefrancq <arnaud.defrancq@laposte.net>
2023-11-07 17:12:49 +0100

    Exo Géo POO / C#

---
+++ 03 - Interface/C#/ExerciceGeo/ExerciceGeo/Cercle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciceGeo
+{
+	internal class Cercle
+	{
+		// Propriete
+		public Double Diametre { get; set; }
+		public Double Rayon { get; set; }
+
+		// Constructeur
+		public Cercle(Double diametre)
+		{
+			Diametre = diametre;
+			Rayon = Diametre / 2;
+		}
+
+		// Methode
+		// Affichage
+		//public override string ToString()
+		//{
+		//	String aff = "";
+		//	aff += "Diametre :  " + Diametre + " - Périmètre : " + Perimetre() + " - Aire : " + Aire();
+
+		//	return aff;
+		//}
+
+		//// Calcul perimetre
+		//public Double Perimetre()
+		//{
+		//	Double peri;
+
+		//}
+
+		//// Calcul aire
+		//public Double Aire()
+		//{
+
+		//}
+	}
+}
---
+++ 03 - Interface/C#/ExerciceGeo/ExerciceGeo/TriangleRectangle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciceGeo
+{
+	internal class TriangleRectangle
+	{
+		// Propriete
+		public Double Base { get; set; }
+		public Double Hauteur { get; set; }
+
+		// Constructeur
+		public TriangleRectangle(Double @base, Double hauteur)
+		{
+			Base = @base;
+			Hauteur = hauteur;
+		}
+
+		// Methode 
+		// Affichage
+		public override string ToString()
+		{
+			String aff = "";
+			aff += "Base& :  " + Base + " - Hauteur : " + Hauteur + " - Périmètre : " + Perimetre() + " - Aire : " + Aire();
+
+			return aff;
+		}
+
+		// calcul du périmetre
+		public Double Perimetre()
+		{
+			Double peri;
+			Double hypothenuse = Math.Sqrt(Math.Pow(Hauteur, 2) + Math.Pow(Base, 2));
+			peri = Hauteur + Base + hypothenuse;
+			return peri;
+		}
+
+		// Calcul du aire
+		public Double Aire()
+		{
+			Double aire;
+			aire = (Hauteur * Base) / 2;
+
+			return aire;
+
+		}
+	}
+}
