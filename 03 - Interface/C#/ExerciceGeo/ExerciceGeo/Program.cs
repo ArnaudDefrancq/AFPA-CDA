2ab59ec0c4e2eadd8d253f199fa5692132f43896
ArnaudDefrancq <arnaud.defrancq@laposte.net>
2023-11-07 16:45:35 +0100

    Fin exo POO / Rectangle

---
+++ 03 - Interface/C#/ExerciceGeo/ExerciceGeo/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ExerciceGeo;
+class Program
+{
+	static void Main()
+	{
+		Rectangle r = new Rectangle(3, 5);
+		Console.WriteLine(r.ToString());
+	}
+}
---
+++ 03 - Interface/C#/ExerciceGeo/ExerciceGeo/Rectangle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciceGeo
+{
+	internal class Rectangle
+	{
+		// Propriete
+		public int Longeur { get; set; }
+		public int Largeur { get; set; }
+
+		// Constructeur
+		public Rectangle(int longeur, int largeur)
+		{
+			Longeur = longeur;
+			Largeur = largeur;
+		}
+
+		// Methode
+		// Affichage
+		public override string ToString()
+		{
+			String aff = "";
+			aff += "Longueur :  " + Longeur + " - Largeur : " + Largeur + " - Périmètre : " + Perimetre() + " - Aire : " + Aire() + " - c'est un carrée ? : " + (EstCarre() ? "oui" : "non");
+
+			return aff;
+		}
+
+		// Methode pour calcul le perimetre
+		private int Perimetre()
+		{
+			int peri = (Longeur + Largeur) * 2;
+			return peri;
+		}
+
+		// Methode pour calcul l'aire
+		private int Aire()
+		{
+			int aire = (Longeur * Largeur);
+			return aire;
+		}
+
+		// Methode pour savoir si estcarré
+		private bool EstCarre()
+		{
+			return (Longeur == Largeur) ? true : false;
+		}
+	}
+}
