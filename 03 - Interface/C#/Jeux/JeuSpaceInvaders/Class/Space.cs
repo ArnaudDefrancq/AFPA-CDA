a79f7b04517bbecbcdc5e880c16de6aa143d37cf
ArnaudDefrancq <arnaud.defrancq@laposte.net>
2023-11-08 17:13:06 +0100

    Début de l'exo 8 / Space Invaders

---
+++ 03 - Interface/C#/Jeux/JeuSpaceInvaders/Class/Space.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeuSpaceInvaders.Class
+{
+	public class Space
+	{
+		// Propriete
+		public int NbLigne { get; set; }
+		public int NbColonnes { get; set; }
+		public List<Object> Grille { get; set; }
+
+		// Constructeur
+		public Space(int nbLigne, int nbColonnes)
+		{
+			NbLigne = nbLigne;
+			NbColonnes = nbColonnes;
+		}
+
+		// Methode
+		// Affichage
+	}
+}
