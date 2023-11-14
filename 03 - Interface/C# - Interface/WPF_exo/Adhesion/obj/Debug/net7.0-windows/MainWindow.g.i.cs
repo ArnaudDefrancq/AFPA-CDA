ec30702c587e23e2ba041905995c43d3351a2f31
59011-82-04 <59011-82-04@exchange.ad.afpanet>
2023-11-14 17:12:25 +0100

    Finir l'interface opération

--- 03 - Interface/C# - Interface/WPF_exo/Adhesion/obj/Debug/net7.0-windows/MainWindow.g.i.cs
+++ 03 - Interface/C# - Interface/WPF_exo/Adhesion/obj/Debug/net7.0-windows/MainWindow.g.i.cs
@@ -189,7 +189,7 @@ namespace Adhesion {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Adhesion;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Adhesion;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- 03 - Interface/C# - Interface/WPF_exo/Calendier/obj/Debug/net7.0-windows/MainWindow.g.i.cs
+++ 03 - Interface/C# - Interface/WPF_exo/Calendier/obj/Debug/net7.0-windows/MainWindow.g.i.cs
@@ -101,7 +101,7 @@ namespace Calendier {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Calendier;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Calendier;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
