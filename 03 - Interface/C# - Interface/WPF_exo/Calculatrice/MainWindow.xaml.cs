c068cf8461cfc4a6f1e6bc3ae40c4c64934640ee
59011-82-04 <59011-82-04@exchange.ad.afpanet>
2023-11-20 11:24:20 +0100

    Ajout des evenements sur la calculatrice : chiffre et virgule ok

--- 03 - Interface/C# - Interface/WPF_exo/Calculatrice/MainWindow.xaml.cs
+++ 03 - Interface/C# - Interface/WPF_exo/Calculatrice/MainWindow.xaml.cs
@@ -20,10 +20,36 @@ namespace Calculatrice
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+
 		public MainWindow()
 		{
 			InitializeComponent();
+			result.Text = "";
 		}
 
+		private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+		{
+			//Faire apparaitre les tailles dans la sortie
+			((Window)sender).ActualHeight.Dump();
+			((Window)sender).ActualWidth.Dump();
+
+			((Window)sender).Height = 4 / 3 * ((Window)sender).ActualWidth; // garder les proportions
+
+
+
+		}
+
+		private void btnNumb_Click(object sender, RoutedEventArgs e)
+		{
+			result.Text += (String)((Button)sender).Content;
+		}
+
+		private void btnVirgule_Click(object sender, RoutedEventArgs e)
+		{
+			if (!result.Text.Contains(','))
+			{
+				result.Text += (String)((Button)sender).Content;
+			}
+		}
 	}
 }
