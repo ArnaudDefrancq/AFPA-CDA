76624d247bc5bddfdc482a02f8169485249b5e97
59011-82-04 <59011-82-04@exchange.ad.afpanet>
2023-11-21 14:20:57 +0100

    Ajout des elements dans la BDD en JSON

---
+++ 03 - Interface/C# - Interface/CRUD/ExoCRUD/APP/CLASS/Produits.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APP.CLASS
+{
+	public class Produits
+	{
+		public int IdProduit { get; set; }
+		public String LibelleProduit { get; set; }
+		public int Quantite { get; set; }
+		public int PrixUnitaire { get; set; }
+		public static int Compteur { get; set; } = 0;
+
+		public Produits(string libelleProduit, int quantite, int prixUnitaire)
+		{
+			IdProduit = Compteur + 1;
+			LibelleProduit = libelleProduit;
+			Quantite = quantite;
+			PrixUnitaire = prixUnitaire;
+			Compteur++;
+		}
+	}
+}
--- 03 - Interface/C# - Interface/CRUD/ExoCRUD/APP/MainWindow.xaml.cs
+++ 03 - Interface/C# - Interface/CRUD/ExoCRUD/APP/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using APP.CLASS;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +26,34 @@ namespace APP
 		public MainWindow()
 		{
 			InitializeComponent();
+			CreerListeJSON(path);
 		}
+
+		List<Produits> produits = new List<Produits>();
+		String path = "U:\\59011-82-04\\AFPA-CDA\\03 - Interface\\C# - Interface\\CRUD\\ExoCRUD\\APP\\JSON\\DB.json";
+
+		private void CreerListeJSON(String path)
+		{
+			List<Produits> liste = new List<Produits>();
+
+			for (int i = 1; i < 15; i++)
+			{
+				Produits p = new Produits("Produit" + i, i * 2, i * 6);
+				liste.Add(p);
+			}
+
+
+			String json = JsonConvert.SerializeObject(liste);
+
+			File.WriteAllText(path, json);
+		}
+
+
+
+
+
 	}
+
+
+
 }
