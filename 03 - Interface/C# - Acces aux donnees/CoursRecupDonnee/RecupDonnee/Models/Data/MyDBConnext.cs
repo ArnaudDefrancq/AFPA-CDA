770bb3b20dbae7aa5c9c301201b593e84abc9e95
59011-82-04 <59011-82-04@exchange.ad.afpanet>
2023-11-22 17:16:26 +0100

    Ajout des la récup des données avec entity

---
+++ 03 - Interface/C# - Acces aux donnees/CoursRecupDonnee/RecupDonnee/Models/Data/MyDBConnext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySql.EntityFrameworkCore;
+
+namespace RecupDonnee.Models.Data
+{
+	public class MyDBConnext : DbContext
+	{
+		public MyDBConnext(DbContextOptions<MyDBConnext> options) : base(options)
+		{
+		}
+	}
+}
---
+++ 03 - Interface/C# - Acces aux donnees/CoursRecupDonnee/RecupDonnee/Models/Data/PersonneContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace RecupDonnee.Models.Data;
+
+public partial class PersonneContext : DbContext
+{
+    public PersonneContext()
+    {
+    }
+
+    public PersonneContext(DbContextOptions<PersonneContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Personne> Personnes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseMySQL("Name=Default");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Personne>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("personnes");
+
+            entity.HasIndex(e => e.Prenom, "prenom_id");
+
+            entity.Property(e => e.Id)
+                .HasColumnType("int(10) unsigned")
+                .HasColumnName("id");
+            entity.Property(e => e.Adresse)
+                .HasMaxLength(50)
+                .HasColumnName("adresse");
+            entity.Property(e => e.CodePostal)
+                .HasColumnType("int(11)")
+                .HasColumnName("codePostal");
+            entity.Property(e => e.Nom)
+                .HasMaxLength(45)
+                .HasColumnName("nom");
+            entity.Property(e => e.Prenom)
+                .HasMaxLength(20)
+                .HasColumnName("prenom");
+            entity.Property(e => e.Ville)
+                .HasMaxLength(20)
+                .HasColumnName("ville");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
