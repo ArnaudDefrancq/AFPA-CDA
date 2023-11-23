56ceab1fb6291a9306a491304664bf290f4ef701
59011-82-04 <59011-82-04@exchange.ad.afpanet>
2023-11-23 10:35:54 +0100

    ModelToDB ok

---
+++ 03 - Interface/C# - Acces aux donnees/ModelToDb/ModelToDb/Models/Data/MyDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace ModelToDb.Models.Data
+{
+	public class MyDBContext : DbContext
+	{
+		public DbSet<Personnes> Personnes { get; set; }
+
+		public MyDBContext(DbContextOptions<MyDBContext> options) : base(options)
+		{
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.Entity<Personnes>(e => e.Property(o => o.Id).HasColumnName("IdPersonne"));
+
+		}
+	}
+}
---
+++ 03 - Interface/C# - Acces aux donnees/ModelToDb/ModelToDb/Models/Data/Personnes.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace ModelToDb.Models.Data
+{
+	public class Personnes
+	{
+		public int Id { get; set; }
+
+		[MaxLength(50)]
+		public String Prenom { get; set; }
+
+		[MaxLength(50)]
+		public String Nom { get; set; }
+
+
+		public int Age { get; set; }
+	}
+}
