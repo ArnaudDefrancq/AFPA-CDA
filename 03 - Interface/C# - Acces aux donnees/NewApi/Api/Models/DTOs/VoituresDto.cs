6286c5d8da464135f7e8aea3853577d03c1c96d9
59011-82-04 <59011-82-04@exchange.ad.afpanet>
2023-11-27 15:39:21 +0100

    Ajout d'une nouvelle API

---
+++ 03 - Interface/C# - Acces aux donnees/NewApi/Api/Models/DTOs/VoituresDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Api.Models.DTOs
+{
+	public class VoituresDto
+	{
+		public string? Marque { get; set; }
+
+		public string? Model { get; set; }
+
+		public int? Km { get; set; }
+	}
+}
---
+++ 03 - Interface/C# - Acces aux donnees/NewApi/Api/Models/Sercives/VoituresService.cs
@@ -0,0 +1,53 @@
+﻿
+namespace Api.Models.Data.Services
+{
+	public class VoituresService
+	{
+		private readonly VoituresDbContext _context;
+
+		public VoituresService(VoituresDbContext context)
+		{
+			_context = context;
+		}
+
+		// Ajout d'une personne dans la base de donnée
+		public void AddVoitures(Voiture v)
+		{
+			if (v == null)
+			{
+				throw new ArgumentNullException(nameof(v));
+			}
+			_context.Voitures.Add(v);
+			_context.SaveChanges();
+		}
+
+		// Suppr une personne dans la base de donnée
+		public void DeleteVoiture(Voiture v)
+		{
+			if (v == null)
+			{
+				throw new ArgumentNullException(nameof(v));
+			}
+			_context.Voitures.Remove(v);
+			_context.SaveChanges();
+		}
+
+		// Donner la liste des personnes en base de donnée
+		public IEnumerable<Voiture> GetAllVoitures()
+		{
+			return _context.Voitures.ToList();
+		}
+
+		// Donne une personne en particulier
+		public Voiture GetVoitureById(int id)
+		{
+			return _context.Voitures.FirstOrDefault(v => v.IdVoiture == id);
+		}
+
+		// Permet de update une personne
+		public void UpdateVoiture(Voiture v)
+		{
+			_context.SaveChanges();
+		}
+	}
+}
