1405ed4c88d2c5c55a80589b6e7e80f939831b25
59011-82-04 <59011-82-04@exchange.ad.afpanet>
2023-11-27 10:06:15 +0100

    API 2 Tables / Ajout des ctrl OK (Pas encore vérifier si elles fonctionnent)

---
+++ 03 - Interface/C# - Acces aux donnees/Exo2Table/Api/Constroller/EmployesController.cs
@@ -0,0 +1,121 @@
+﻿
+using Api.Data.DTO;
+using Api.Data.Models;
+using Api.Data.Service;
+using AutoMapper;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.JsonPatch.Adapters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Api.Controllers
+{
+	[Route("api/[Controller]")]
+	[ApiController]
+	public class EmployesController : ControllerBase
+	{
+		private readonly EmployeServices _service;
+		private readonly IMapper _mapper;
+
+		public EmployesController(EmployeServices service, IMapper mapper)
+		{
+			_service = service;
+			_mapper = mapper;
+		}
+
+		// GET api/personnes
+		[HttpGet]
+		public ActionResult<IEnumerable<EmployeDTO>
+			> GetAllPersonne()
+		{
+			var listePersonnes = _service.GetAllPersonnes();
+			return Ok(_mapper.Map<IEnumerable<EmployeDTO>
+				>(listePersonnes));
+		}
+
+		// GET  api/personnes/{id}
+		[HttpGet("{id}", Name = "GetPersonneById")]
+		public ActionResult<EmployeDTO>
+			GetPersonneById(int id)
+		{
+			var commandItem = _service.GetPersonneById(id);
+			if (commandItem != null)
+			{
+				return Ok(_mapper.Map<EmployeDTO>
+					(commandItem));
+			}
+			return NotFound();
+		}
+
+		// POST api/Personnes
+		[HttpPost]
+		public ActionResult<EmployeDTO>
+			CreatePersonne(Employe employe)
+		{
+			//on ajoute l’objet à la base de données
+			_service.AddPersonnes(employe);
+			//on retourne le chemin de findById avec l'objet créé
+			return CreatedAtRoute(nameof(GetPersonneById), new { employe.IdEmploye }, employe);
+		}
+
+		// PUT api/personnes/{id}
+		[HttpPut("{id}")]
+		public ActionResult UpdatePersonne(int id, EmployeDTO employe)
+		{
+			var employeFromRepo = _service.GetPersonneById(id);
+			if (employeFromRepo == null)
+			{
+				return NotFound();
+			}
+			employeFromRepo.Dump();
+			_mapper.Map(employe, employeFromRepo);
+			employeFromRepo.Dump();
+
+			_service.UpdatePersonne(employeFromRepo);
+
+			return NoContent();
+		}
+
+		// PATCH api/personnes/{id}
+		[HttpPatch("{id}")]
+		public ActionResult PartialPersonneUpdate(int id, JsonPatchDocument<Employe>
+			patchDoc)
+		{
+			var employeFromRepo = _service.GetPersonneById(id);
+			if (employeFromRepo == null)
+			{
+				return NotFound();
+			}
+
+			var employeToPatch = _mapper.Map<Employe>(employeFromRepo);
+
+			patchDoc.ApplyTo(employeFromRepo, ModelState);
+
+			if (!TryValidateModel(employeToPatch))
+			{
+				return ValidationProblem(ModelState);
+			}
+			employeFromRepo.Dump();
+			_mapper.Map(employeToPatch, employeFromRepo);
+			employeFromRepo.Dump();
+			_service.UpdatePersonne(employeFromRepo);
+
+			return NoContent();
+		}
+
+		// DELETE api/personnes/{id}
+		[HttpDelete("{id}")]
+		public ActionResult DeletePersonne(int id)
+		{
+			var employeModelFromRepo = _service.GetPersonneById(id);
+			if (employeModelFromRepo == null)
+			{
+				return NotFound();
+			}
+
+			_service.DeletePersonne(employeModelFromRepo);
+			return NoContent();
+		}
+	}
+}
+
---
+++ 03 - Interface/C# - Acces aux donnees/Exo2Table/Api/Constroller/VoitureFonctionsController.cs
@@ -0,0 +1,119 @@
+﻿using Api.Data;
+using Api.Data.DTO;
+using Api.Data.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.JsonPatch.Adapters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Api.Controllers
+{
+	[Route("api/[Controller]")]
+	[ApiController]
+	public class VoitureFonctionsController : ControllerBase
+	{
+		private readonly VoitureFonctionServices _service;
+		private readonly IMapper _mapper;
+
+		public VoitureFonctionsController(VoitureFonctionServices service, IMapper mapper)
+		{
+			_service = service;
+			_mapper = mapper;
+		}
+
+		// GET api/personnes
+		[HttpGet]
+		public ActionResult<IEnumerable<VoitureFonctionDTO>
+			> GetAllPersonne()
+		{
+			var listePersonnes = _service.GetAllPersonnes();
+			return Ok(_mapper.Map<IEnumerable<VoitureFonctionDTO>
+				>(listePersonnes));
+		}
+
+		// GET  api/personnes/{id}
+		[HttpGet("{id}", Name = "GetPersonneById")]
+		public ActionResult<VoitureFonctionDTO>
+			GetPersonneById(int id)
+		{
+			var commandItem = _service.GetPersonneById(id);
+			if (commandItem != null)
+			{
+				return Ok(_mapper.Map<VoitureFonctionDTO>
+					(commandItem));
+			}
+			return NotFound();
+		}
+
+		// POST api/Personnes
+		[HttpPost]
+		public ActionResult<VoitureFonctionDTO>
+			CreatePersonne(Voiturefonction voiture)
+		{
+			//on ajoute l’objet à la base de données
+			_service.AddPersonnes(voiture);
+			//on retourne le chemin de findById avec l'objet créé
+			return CreatedAtRoute(nameof(GetPersonneById), new { voiture.IdVoitureFonction }, voiture);
+		}
+
+		// PUT api/personnes/{id}
+		[HttpPut("{id}")]
+		public ActionResult UpdatePersonne(int id, VoitureFonctionDTO voiture)
+		{
+			var voitureFromRepo = _service.GetPersonneById(id);
+			if (voitureFromRepo == null)
+			{
+				return NotFound();
+			}
+			voitureFromRepo.Dump();
+			_mapper.Map(voiture, voitureFromRepo);
+			voitureFromRepo.Dump();
+
+			_service.UpdatePersonne(voitureFromRepo);
+
+			return NoContent();
+		}
+
+		// PATCH api/personnes/{id}
+		[HttpPatch("{id}")]
+		public ActionResult PartialPersonneUpdate(int id, JsonPatchDocument<Voiturefonction>
+			patchDoc)
+		{
+			var voitureFromRepo = _service.GetPersonneById(id);
+			if (voitureFromRepo == null)
+			{
+				return NotFound();
+			}
+
+			var voitureToPatch = _mapper.Map<Voiturefonction>(voitureFromRepo);
+
+			patchDoc.ApplyTo(voitureFromRepo, ModelState);
+
+			if (!TryValidateModel(voitureToPatch))
+			{
+				return ValidationProblem(ModelState);
+			}
+			voitureFromRepo.Dump();
+			_mapper.Map(voitureToPatch, voitureFromRepo);
+			voitureFromRepo.Dump();
+			_service.UpdatePersonne(voitureFromRepo);
+
+			return NoContent();
+		}
+
+		// DELETE api/personnes/{id}
+		[HttpDelete("{id}")]
+		public ActionResult DeletePersonne(int id)
+		{
+			var voitureModelFromRepo = _service.GetPersonneById(id);
+			if (voitureModelFromRepo == null)
+			{
+				return NotFound();
+			}
+
+			_service.DeletePersonne(voitureModelFromRepo);
+			return NoContent();
+		}
+	}
+}
