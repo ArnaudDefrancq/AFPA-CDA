32a3dd43fa0c6528d2c8eed14011f38f196c3be3
59011-82-04 <59011-82-04@exchange.ad.afpanet>
2023-12-06 17:04:39 +0100

    Init WPF + Scaffold

---
+++ 03 - Interface/C# - Interface/CRUD/CRUDAvecScaffold/CRUDVoitureDb/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CRUDVoitureDb
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
+// A mettre dans le appsetting.json
+// DbName = DbName
+
+//"ConnectionStrings": {
+//	"Default": "Server=localhost;User=root;Database=DbName;Port=3306;SslMode=None;"
+//	}
+
+
+// Le scaffold
+// scaffold-DbContext -Connection Name=Default -ProviderMySql.EntityFrameworkCore -OutpuDir Models/Data -Context DbNameDBContext -ContextDir Models
+
+
---
+++ 03 - Interface/C# - Interface/CRUD/CRUDAvecScaffold/CRUDVoitureDb/Models/DbNameDBContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using CRUDVoitureDb.Models.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDVoitureDb.Models;
+
+public partial class DbNameDBContext : DbContext
+{
+	public DbNameDBContext()
+	{
+	}
+
+	public DbNameDBContext(DbContextOptions<DbNameDBContext> options)
+		: base(options)
+	{
+	}
+
+	public virtual DbSet<Voiture> Voitures { get; set; }
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseMySQL("server=localhost;user=root;database=voituredb;port=3306;ssl mode=none");
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.Entity<Voiture>(entity =>
+		{
+			entity.HasKey(e => e.IdVoiture).HasName("PRIMARY");
+
+			entity.ToTable("voiture");
+
+			entity.Property(e => e.IdVoiture)
+				.HasColumnType("int(11)")
+				.HasColumnName("Id_Voiture");
+			entity.Property(e => e.Km)
+				.HasColumnType("int(11)")
+				.HasColumnName("km");
+			entity.Property(e => e.Marque)
+				.HasMaxLength(50)
+				.HasColumnName("marque");
+			entity.Property(e => e.Model)
+				.HasMaxLength(50)
+				.HasColumnName("model");
+		});
+
+		OnModelCreatingPartial(modelBuilder);
+	}
+
+	partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
