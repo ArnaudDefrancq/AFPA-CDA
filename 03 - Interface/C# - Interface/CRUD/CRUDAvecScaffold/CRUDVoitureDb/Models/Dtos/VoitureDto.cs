10c8add0f37375d9a09a6a30035e86f3cd6f6591
59011-82-04 <59011-82-04@exchange.ad.afpanet>
2023-12-07 09:38:13 +0100

    Ajout de la logique des btns page DetailsVoiture OK

--- 03 - Interface/C# - Interface/CRUD/CRUDAvecScaffold/CRUDVoitureDb/Models/Dtos/VoitureDto.cs
+++ 03 - Interface/C# - Interface/CRUD/CRUDAvecScaffold/CRUDVoitureDb/Models/Dtos/VoitureDto.cs
@@ -2,14 +2,16 @@
 {
 	public class VoitureDto
 	{
+		public int IdVoiture { get; set; }
 		public string? Marque { get; set; }
 
 		public string? Model { get; set; }
 
 		public int? Km { get; set; }
 
-		public VoitureDto(string? marque, string? model, int? km)
+		public VoitureDto(int idVoiture, string? marque, string? model, int? km)
 		{
+			IdVoiture = idVoiture;
 			Marque = marque;
 			Model = model;
 			Km = km;
--- 03 - Interface/C# - Interface/CRUD/CRUDAvecScaffold/CRUDVoitureDb/View/DetailsVoiture.xaml.cs
+++ 03 - Interface/C# - Interface/CRUD/CRUDAvecScaffold/CRUDVoitureDb/View/DetailsVoiture.xaml.cs
@@ -1,4 +1,7 @@
-﻿using CRUDVoitureDb.Models.Dtos;
+﻿using CRUDVoitureDb.Controllers;
+using CRUDVoitureDb.Models;
+using CRUDVoitureDb.Models.Data;
+using CRUDVoitureDb.Models.Dtos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,15 +26,116 @@ namespace CRUDVoitureDb.View
 		public bool validAjoutArticle = false;
 		public bool validModifArticle = false;
 
+		private VoitureController _controller;
+		private VoitureDBContext _context;
+
+
 		public string Mode { get; set; }
 		public MainWindow Mw { get; set; }
 
 		public DetailsVoiture(VoitureDto v, MainWindow w, string mode)
 		{
 			InitializeComponent();
+
+			_context = new VoitureDBContext();
+			_controller = new VoitureController(_context);
+
 			Mw = w;
 			Mode = mode;
 			btnValide.Content = Mode;
+		}
+
+		//*******************************************************//
+		// Permet de remplire les inputs quand on veut modifier ou supprimer
+		public void DisplayInput(VoitureDto v)
+		{
+			if (Mode != "Ajouter") // On ecrit dans les inputs les valeurs
+			{
+				txtMarque.Text = v.Marque;
+				txtIdVoiture.Text = v.IdVoiture.ToString();
+				txtModel.Text = v.Model;
+				txtNbKm.Text = v.Km.ToString();
+
+				// Si supprimer, les inputs ne peuvent pas être modifier
+				if (Mode == "Supprimer")
+				{
+					txtMarque.IsEnabled = false;
+					txtIdVoiture.IsEnabled = false;
+					txtModel.IsEnabled = false;
+					txtNbKm.IsEnabled = false;
+				}
+			}
+			else
+			{
+				txtIdVoiture.Text = "0";
+			}
+		}
+
+		//*******************************************************//
+		// Action du btn valider en fonction des btns Ajouts/Modifier/Supprimer
+		private void valider_Click(object sender, RoutedEventArgs e)
+		{
+			VoitureDto v = new VoitureDto(Int32.Parse((string)txtIdVoiture.Text), txtMarque.Text, txtModel.Text, Int32.Parse(txtNbKm.Text));
+
+			switch (Mode)
+			{
+				case "Ajouter": VoitureController.CreateVoiture(v); break;
+				case "Modifier": VoitureController.UpdateVoiture(v); break;
+				case "Supprimer": VoitureController.DeleteVoiture(v); break;
+			}
+			this.Close();
+		}
+
+		//*******************************************************//
+		// Action de fermer la page
+		private void annuler_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
+
+		//*******************************************************//
+		// Activation du button si les inputs sont correctes
+		private void TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+		{
+			// Recup des données dans les inputs
+			int km;
+			String marque = txtMarque.Text;
+			String model = txtModel.Text;
+			String valueKm = txtNbKm.Text;
+
+
+			// Vérification des données pour Ajout
+			if (int.TryParse(txtNbKm, out km) && (marque = txtMarque.Text).Length > 0 && (model = txtModel.Text).Length > 0)
+			{
+				validAjoutArticle = true;
+				validModifArticle = true;
+				BtnActivationDesactivation();
+
+			}
+			else
+			{
+				validAjoutArticle = false;
+				validModifArticle = false;
+				BtnActivationDesactivation();
+
+			}
+		}
+
+		//*******************************************************//
+		// Activation du button en fonction
+		private void BtnActivationDesactivation()
+		{
+			if (validAjoutArticle || validModifArticle)
+			{
+				btnValide.IsEnabled = true;
+				Style dynamicStyle = (Style)Application.Current.Resources["btnTemp"];
+				btnValide.Style = dynamicStyle;
+			}
+			else
+			{
+				btnValide.IsEnabled = false;
+				btnValide.Style = null;
+			}
 		}
 	}
 }
