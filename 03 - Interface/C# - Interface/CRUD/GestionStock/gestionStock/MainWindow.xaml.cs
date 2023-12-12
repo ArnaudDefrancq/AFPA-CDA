c118f5ddd41e1aad456b5c355ebbb2de9963aca7
59011-82-04 <59011-82-04@exchange.ad.afpanet>
2023-12-12 11:06:28 +0100

    Ajout les View/Liste pour chaque partie OK

--- 03 - Interface/C# - Interface/CRUD/GestionStock/gestionStock/MainWindow.xaml.cs
+++ 03 - Interface/C# - Interface/CRUD/GestionStock/gestionStock/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using gestionStock.Models;
+using gestionStock.Models.Data;
+using gestionStock.View;
+using gestionStock.View.Listes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +24,42 @@ namespace gestionStock
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+
+		public GestionStocksDBContext _context;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+			_context = new GestionStocksDBContext();
+		}
+
+
+		//*******************************************************//
+		// Action sur les btns Categorie/Produits/TypesProduits
+		private void BtnActionClick(object sender, RoutedEventArgs e)
+		{
+			switch (((Button)sender).Name)
+			{
+				case "btnArticle":
+					ArticleListe articleListe = new ArticleListe();
+					this.Opacity = 0.7;
+					articleListe.ShowDialog();
+					this.Opacity = 1;
+					break;
+				case "btnCategories":
+					CategorieListe categorieListe = new CategorieListe();
+					this.Opacity = 0.7;
+					categorieListe.ShowDialog();
+					this.Opacity = 1;
+					break;
+				case "btnTypeProduit":
+					TypesProduitListe typesProduitListe = new TypesProduitListe();
+					this.Opacity = 0.7;
+					typesProduitListe.ShowDialog();
+					this.Opacity = 1;
+					break;
+			}
+
 		}
 	}
 }
