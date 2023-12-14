bbf68c0c469aaa3635610d20eb13c38a9b94e5e2
unknown <arnaud.defrancq@laposte.net>
2023-12-14 09:14:08 +0100

    Affichage des inscriptions OK

--- 03 - Interface/C# - Interface/CRUD/GestionCours/CRUDGestionCours/MainWindow.xaml.cs
+++ 03 - Interface/C# - Interface/CRUD/GestionCours/CRUDGestionCours/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using CRUDGestionCours.Controllers;
+using CRUDGestionCours.Models;
+using CRUDGestionCours.Models.Data;
+using CRUDGestionCours.Models.Dtos;
+using CRUDGestionCours.View.Formulaire;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,10 +26,108 @@ namespace CRUDGestionCours
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		public GestionCoursDBContext _context;
+		private InscriptionController _controllerInscription;
+		public bool validModif = false;
+		public bool validSuppr = false;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+			_context = new GestionCoursDBContext();
+			_controllerInscription = new InscriptionController(_context);
+			DisplayDataGridInsciption();
 		}
+
+		//*******************************************************//
+		//Affiche dans le dataGRid
+		public void DisplayDataGridInsciption()
+		{
+			gridDataInscription.ItemsSource = _controllerInscription.GetAllInscriptions();
+		}
+
+		//*******************************************************//
+		// Permet d'activer les btn suppr et modifier
+		private void BtnActiveDesactiveSuppr()
+		{
+			if (validSuppr)
+			{
+				btnSuppr.IsEnabled = true;
+			}
+			else
+			{
+				btnSuppr.IsEnabled = false;
+			}
+		}
+
+		public void BtnActiveDesactiveModif()
+		{
+			if (validModif)
+			{
+				btnModifier.IsEnabled = true;
+				Style dynamicStyle = (Style)Application.Current.Resources["btnStyle"];
+				btnModifier.Style = dynamicStyle;
+			}
+			else
+			{
+				btnModifier.IsEnabled = false;
+				btnModifier.Style = null;
+			}
+		}
+
+		//************************************************//
+		// Permet d'activer les btn suppr et modifier au click sur un element de la dataGrid
+		private void gridDataArticle_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			validSuppr = true;
+			validModif = true;
+
+			BtnActiveDesactiveModif();
+			BtnActiveDesactiveSuppr();
+		}
+
+		//************************************************//
+		// Action sur les btns Ajouts/Modifier/Supprimer
+		private void btnActionClick(object sender, EventArgs e)
+		{
+			InscriptionDtoOutAplatie item;
+			if (((Button)sender).Name == "btnAjouter")
+			{
+				item = new InscriptionDtoOutAplatie();
+			}
+			else
+			{
+				item = (InscriptionDtoOutAplatie)gridDataInscription.SelectedItem;
+			}
+
+			InscriptionFormulaire w = new InscriptionFormulaire(item, this, (string)((Button)sender).Content);
+			this.Opacity = 0.7;
+			w.ShowDialog();
+			this.Opacity = 1;
+			DisplayDataGridInsciption();
+			validSuppr = false;
+			validModif = false;
+			BtnActiveDesactiveModif();
+			BtnActiveDesactiveSuppr();
+		}
+
+		//************************************************//
+		// Action sur les btns Ajouts/Modifier/Supprimer
+		private void Row_DoubleClick(object sender, EventArgs e)
+		{
+			InscriptionDtoOutAplatie item = gridDataInscription.SelectedItem as InscriptionDtoOutAplatie;
+
+			InscriptionFormulaire w = new InscriptionFormulaire(item, this, "Modifier");
+			this.Opacity = 0.7;
+			w.ShowDialog();
+			this.Opacity = 1;
+			DisplayDataGridInsciption();
+			validSuppr = false;
+			validModif = false;
+			BtnActiveDesactiveModif();
+			BtnActiveDesactiveSuppr();
+		}
+
 	}
 }
 
---
+++ 03 - Interface/C# - Interface/CRUD/GestionCours/CRUDGestionCours/View/Formulaire/InscriptionFormulaire.xaml.cs
@@ -0,0 +1,31 @@
+﻿using CRUDGestionCours.Models.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CRUDGestionCours.View.Formulaire
+{
+	/// <summary>
+	/// Logique d'interaction pour InscriptionFormulaire.xaml
+	/// </summary>
+	public partial class InscriptionFormulaire : Window
+	{
+		public string Mode { get; set; }
+		public MainWindow Mw { get; set; }
+
+		public InscriptionFormulaire(InscriptionDtoOutAplatie ia, MainWindow w, string mode)
+		{
+			InitializeComponent();
+		}
+	}
+}
