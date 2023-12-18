83db3f810fe25ac0be27543a65634566463d39be
59011-82-04 <59011-82-04@exchange.ad.afpanet>
2023-12-18 12:01:52 +0100

    Afpa-Midi

--- 03 - Interface/C# - Interface/CRUD/GestionStock/GestionStockTestUnit/ControllerTest.cs
+++ 03 - Interface/C# - Interface/CRUD/GestionStock/GestionStockTestUnit/ControllerTest.cs
@@ -7,8 +7,37 @@ namespace GestionStockTestUnit
 		{
 		}
 
+		private GestionStockDBContext()
+		{
+
+		}
+
 		[Test]
-		public void Test1()
+		public void Article_GetAllArticlesTest()
+		{
+
+			Assert.Pass();
+		}
+		[Test]
+		public void Article_GetArticleByIDTest()
+		{
+
+			Assert.Pass();
+		}
+		[Test]
+		public void Article_CreateArticleTest()
+		{
+
+			Assert.Pass();
+		}
+		[Test]
+		public void Article_UpdateArticleTest()
+		{
+
+			Assert.Pass();
+		}
+		[Test]
+		public void Article_DeleteArticleTest()
 		{
 			Assert.Pass();
 		}
